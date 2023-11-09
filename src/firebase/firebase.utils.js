8b4c543b43d3c82eba13fe90f8b01401e0c3dc49
fh-fahad <fahadbin8900@gmail.com>
2023-11-09 22:37:08 +0600

    few design changes

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,22 +12,23 @@ const config = {
   measurementId: "G-VXHW96775F",
 };
 
+firebase.initializeApp(config);
+
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
 
   const userRef = firestore.doc(`users/${userAuth.uid}`);
 
-  const snapshot = await userRef.get();
+  const snapShot = await userRef.get();
 
-  if (!snapshot.exists) {
+  if (!snapShot.exists) {
     const { displayName, email } = userAuth;
-    const creaedAt = new Date();
-
+    const createdAt = new Date();
     try {
       await userRef.set({
         displayName,
         email,
-        creaedAt,
+        createdAt,
         ...additionalData,
       });
     } catch (error) {
@@ -38,7 +39,38 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   return userRef;
 };
 
-firebase.initializeApp(config);
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+
+  const batch = firestore.batch();
+  objectsToAdd.forEach((obj) => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = (collections) => {
+  const transformedCollection = collections.docs.map((doc) => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items,
+    };
+  });
+
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -1,11 +1,19 @@
-import SHOP_DATA from "./shop.data";
+import ShopActionTypes from "./shop.types";
 
 const INITIAL_STATE = {
-  collections: SHOP_DATA,
+  collections: null,
+  isFatching: false,
+  errorMessage: undefined,
 };
 
 const shopReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case ShopActionTypes.FETCH_COLLECTIONS_START:
+      return { ...state, isFatching: true };
+    case ShopActionTypes.FETCH_COLLECTIONS_SUCCESS:
+      return { ...state, isFatching: false, collections: action.payload };
+    case ShopActionTypes.FETCH_COLLECTIONS_FAILURE:
+      return { ...state, isFatching: false, errorMessage: action.payload };
     default:
       return state;
   }
